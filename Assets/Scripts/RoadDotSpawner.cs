361fa684a22982a24f611d81df13428644b27243
Sakan Pengdis <pengdis24@gmail.com>
2024-12-27 23:43:11 -0700

    Added line spawning

---
+++ Assets/Scripts/RoadDotSpawner.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class RoadDotSpawner : MonoBehaviour
+{
+
+    [SerializeField] private int speed;
+    [SerializeField] private float maxSpawnTimer;
+    private float spawnTimer;
+    [SerializeField] private List<GameObject> dotSpawners;
+    [SerializeField] private GameObject dotPrefab;
+    private void Awake()
+    {
+        
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(spawnTimer < 0)
+        {
+            foreach(GameObject spawner in dotSpawners)
+            {
+                GameObject tempdot = Instantiate(dotPrefab, spawner.transform);
+                tempdot.GetComponent<Rigidbody2D>().linearVelocity = new Vector2(0, -1f * speed);
+            }
+            spawnTimer = maxSpawnTimer;
+        } else
+        {
+            spawnTimer -= Time.deltaTime;
+        }
+    }
+
+
+}
