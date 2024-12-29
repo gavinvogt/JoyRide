61aa062d4efb311bee190dc3d7d641d080e44fd9
Josh <joshua.zimmerling@gmail.com>
2024-12-28 17:05:06 -0700

    Starting UI code

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -11,12 +11,31 @@ public class UI : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        healthGauge.transform.rotation = Quaternion.Euler(0, 0, 0);
+        ammoGauge.transform.rotation = Quaternion.Euler(0, 0, 0);
+        speedGauge.transform.rotation = Quaternion.Euler(0, 0, 90);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void updateUI(GameObject player)
     {
-        
+        Player playerScript = player.GetComponent<Player>();
+        updateHealth(playerScript.GetCar(), playerScript.GetCar());
+        updateAmmo(1);
+        updateSpeed(1);
+    }
+
+    public void updateHealth(int currentHealth, int maxHealth)
+    {
+        float healthPercentage = (float) currentHealth / (float) maxHealth;
+        float angleOfGauge = -90f * (1 - healthPercentage);
+        healthGauge.transform.rotation = Quaternion.Euler(0, 0, angleOfGauge);
+    }
+    public void updateAmmo(int ammo)
+    {
+
+    }
+    public void updateSpeed(int speed)
+    {
+
     }
 }
