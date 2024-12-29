fca70d4148b483919ff4bdc9cab02d7a9906c739
Gavin <gavinvogt7@gmail.com>
2024-12-28 22:15:41 -0800

    Add slight bullet spread to machine gun

--- Assets/Scripts/Gun/MachineGun.cs
+++ Assets/Scripts/Gun/MachineGun.cs
@@ -3,13 +3,16 @@ using UnityEngine;
 
 public class MachineGun : Gun
 {
+    // configure machine gun bullet spread
+    [SerializeField] private float bulletSpreadFactorAngle;
+
     override public IEnumerator Fire()
     {
         if (car.getCurrentAmmo() > 0)
         {
             car.useAmmo();
             player.GetComponent<Player>().updatePlayerUI();
-            Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+            Instantiate(bulletPrefab, firePoint.position, GetBulletAngle());
             yield return new WaitForSeconds(fireCooldown);
             if (Input.GetButton("Fire1"))
             {
@@ -17,5 +20,13 @@ public class MachineGun : Gun
                 StartCoroutine(Fire());
             }
         }
+    }
+
+    private Quaternion GetBulletAngle()
+    {
+        return firePoint.rotation * Quaternion.AngleAxis(
+            Random.Range(-bulletSpreadFactorAngle, bulletSpreadFactorAngle),
+            Vector3.forward
+        );
     }
 }
