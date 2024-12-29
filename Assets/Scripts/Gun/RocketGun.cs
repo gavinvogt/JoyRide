3ab3ada6cd5c6710c3c0f3c11242973c36d01f5a
Gavin <gavinvogt7@gmail.com>
2024-12-28 22:11:21 -0800

    Add rocket launcher script for tank gun

---
+++ Assets/Scripts/Gun/RocketGun.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using UnityEngine;
+
+public class RocketGun : Gun
+{
+    // configure how the rocket launcher shoots ... rockets
+    [SerializeField] private float rocketSpreadFactorAngle;
+    private bool canFire = true;
+
+    override public IEnumerator Fire()
+    {
+        if (canFire && car.getCurrentAmmo() > 0)
+        {
+            canFire = false;
+            car.useAmmo();
+            player.GetComponent<Player>().updatePlayerUI();
+
+            // Fire the rocket
+            Instantiate(bulletPrefab, firePoint.position, GetBulletAngle());
+
+            // Allow firing after cooldown
+            yield return new WaitForSeconds(fireCooldown);
+            canFire = true;
+        }
+    }
+
+    private Quaternion GetBulletAngle()
+    {
+        return firePoint.rotation * Quaternion.AngleAxis(
+            Random.Range(-rocketSpreadFactorAngle, rocketSpreadFactorAngle),
+            Vector3.forward
+        );
+    }
+}
