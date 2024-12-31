0f6cb052f9feb25dfd6e7a66fb320103f9e46506
Sakan Pengdis <56180036+thatperson24@users.noreply.github.com>
2024-12-30 17:35:00 -0700

    Added new tags for laser

--- Assets/Scripts/Constants/ObjectTags.cs
+++ Assets/Scripts/Constants/ObjectTags.cs
@@ -3,10 +3,11 @@ public class ObjectTags
     public readonly static string OBSTACLE_BLOCKING = "ObstacleBlocking";
     public readonly static string OBSTACLE_NON_BLOCKING = "ObstacleNonBlocking";
     public readonly static string INDESTRUCTABLE_OBSTACLE = "IndestructableObstacle";
+    public readonly static string INDESTRUCTABLE_OBSTACLE_NON_BLOCKING = "IndestructableObstacleNonBlocking";
 
     public static bool IsObstacle(string tag)
     {
-        return tag == OBSTACLE_BLOCKING || tag == OBSTACLE_NON_BLOCKING || tag == INDESTRUCTABLE_OBSTACLE;
+        return tag == OBSTACLE_BLOCKING || tag == OBSTACLE_NON_BLOCKING || tag == INDESTRUCTABLE_OBSTACLE || tag == INDESTRUCTABLE_OBSTACLE_NON_BLOCKING;
     }
 
     public static bool IsBlockingObstacle(string tag)
