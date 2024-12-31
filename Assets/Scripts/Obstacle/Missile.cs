af3b1eba45326f8d4a05192dea7f1be8dc15cebe
Josh <joshua.zimmerling@gmail.com>
2024-12-30 19:14:12 -0700

    Adding explosion prefab

--- Assets/Scripts/Obstacle/Missile.cs
+++ Assets/Scripts/Obstacle/Missile.cs
@@ -6,6 +6,7 @@ public class Missile : MonoBehaviour
     private Transform targetPos;
     private Spotlight spotlight;
     [SerializeField] private AudioClip hitSoundClip;
+    [SerializeField] private GameObject explosion;
 
     // Update is called once per frame
     void FixedUpdate()
@@ -27,7 +28,10 @@ public class Missile : MonoBehaviour
     {
         gameObject.GetComponent<CircleCollider2D>().enabled = true;
         SoundFXManager.instance.PlaySoundFXClip(hitSoundClip, transform, 1f);
-        yield return new WaitForSeconds(1f);
+        //GameObject hitExplosion = Instantiate(explosion, new Vector3(this.transform.position.x, this.transform.position.y, -1.5f), this.transform.rotation, this.transform);
+        yield return new WaitForSeconds(0.5f);
+        //Destroy(hitExplosion);
+        yield return new WaitForSeconds(0.5f);
         spotlight.StartCoroutine(spotlight.Move());
         Destroy(gameObject);
     }
