ab80c2665596b129c2996accef508aa635650e15
Gavin <gavinvogt7@gmail.com>
2025-04-12 18:38:48 -0700

    Add classes for tracking car stats and upgrading

---
+++ Assets/Scripts/Constants/CarLevels/CarStats.cs
@@ -0,0 +1,28 @@
+public readonly struct CarStats
+{
+    public double Speed { get; init; }
+    public double Health { get; init; }
+    public int Ammo { get; init; }
+    public double Damage { get; init; }
+    public int BulletsPerShot { get; init; }
+    /** Time between each attack, in seconds */
+    public double AttackRate { get; init; }
+    public double Spread { get; init; }
+
+    public CarStats(double speed, double health, int ammo, double damage, double attackRate, double spread) {
+        this(speed, health, ammo, damage, 1, attackRate, spread);
+    }
+
+    public CarStats(double speed, double health, int ammo, double damage, int bulletsPerShot, double attackRate, double spread) {
+        Speed = speed;
+        Health = health;
+        Ammo = ammo;
+        Damage = damage;
+        BulletsPerShot = bulletsPerShot;
+        AttackRate = attackRate;
+        Spread = spread;
+    }
+
+    public override string ToString() =>
+        $"(Speed={Speed}, Health={Health}, Ammo={Ammo}, Damage={Damage}, BulletsPerShot={BulletsPerShot}, AttackRate={AttackRate}), Spread={Spread}";
+}
