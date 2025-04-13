d0adac1ef249221c4c7f0f8cd39d052a32dc5858
Josh <joshua.zimmerling@gmail.com>
2025-04-13 13:11:28 -0400

    Removing this constructor cause it was causing me compil problems

--- Assets/Scripts/Constants/CarLevels/CarStats.cs
+++ Assets/Scripts/Constants/CarLevels/CarStats.cs
@@ -10,7 +10,13 @@ public readonly struct CarStats
     public double Spread { get; init; }
 
     public CarStats(double speed, double health, int ammo, double damage, double attackRate, double spread) {
-        this(speed, health, ammo, damage, 1, attackRate, spread);
+        Speed = speed;
+        Health = health;
+        Ammo = ammo;
+        Damage = damage;
+        BulletsPerShot = 1;
+        AttackRate = attackRate;
+        Spread = spread;
     }
 
     public CarStats(double speed, double health, int ammo, double damage, int bulletsPerShot, double attackRate, double spread) {
